cbd5f76eb84ba3fd59ac9c3b86ff0fb9a9a10882
Marcos Romero <marcos.romero.lamas@cern.ch>
2019-11-06 17:35:10 +0100

    Create test1.py to compute angular acceptance and imput/parameters 2

--- angular-acceptance/test1.py
+++ angular-acceptance/test1.py
@@ -3,20 +3,7 @@
 
 
 
-# Imports ----------------------------------------------------------------------
-import sys
-sys.path.append("../")
-#from ipanema import Parameters, fit_report, minimize
-import pyopencl as cl  # Import the OpenCL GPU computing API
-import pyopencl.array as pycl_array  # Import PyOpenCL Array (a Numpy array plus an OpenCL buffer object)
-import numpy as np  # Import Numpy number tools
-import matplotlib.pyplot as plt
-import corner
-
-import os
-os.environ['PYOPENCL_COMPILER_OUTPUT'] = '1'
-os.environ['PYOPENCL_CTX'] = '1
-
+# %% Modules -------------------------------------------------------------------
 import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
@@ -24,109 +11,195 @@ import pandas as pd
 from sympy.physics.quantum.spin import Rotation
 from sympy.abc import i, k, l, m
 import math
-#import uproot
+import uproot
 import os
 import platform
 
 
-# import pycuda.driver as cuda
-# import pycuda.autoinit
-# import pycuda.gpuarray as gpuarray
-# from pycuda.compiler import SourceModule
+import pycuda.driver as cuda
+import pycuda.autoinit
+import pycuda.gpuarray as gpuarray
+from pycuda.compiler import SourceModule
+import json
+
+BLOCK_SIZE = 5#56
+MAX_EVENTS = 5
+
+def getGrid(Nevts, BLOCK_SIZE):
+  Nbunch = Nevts *1. / BLOCK_SIZE
+  if Nbunch > int(Nbunch):
+    Nbunch = int(Nbunch) +1
+  else :
+    Nbunch = int(Nbunch)
+  return Nbunch
 
 
-# %% Prepare context: were OpenCL function should run --------------------------
-context = cl.Context([cl.get_platforms()[0].get_devices()[1]])
-queue = cl.CommandQueue(context)  # Instantiate a Queue
-
-
-
-subs_dict = {
-  "APlon":      np.float64(0.7217582697828963),
-  "ASlon":      np.float64(0.0),
-  "APpar":      np.float64(0.47983226235842036),
-  "APper":      np.float64(0.4988246184782784),
-  "CSP":        np.float64(1),
-  "DG":         np.float64(0.0),
-  "DM":         np.float64(17.8),
-  "G":          np.float64(0.66137),
-  "deltaPlon":  np.float64(0.0),
-  "deltaSlon":  np.float64(3.07),
-  "deltaPpar":  np.float64(3.3),
-  "deltaPper":  np.float64(3.07),
-  "lPlon":      np.float64(1),
-  "lSlon":      np.float64(1),
-  "lPpar":      np.float64(1),
-  "lPper":      np.float64(1),
-  "phisPlon":   np.float64(0.07),
-  "phisSlon":   np.float64(0.07),
-  "phisPpar":   np.float64(0.07),
-  "phisPper":   np.float64(0.07)
-}
+# %% Load parameters -----------------------------------------------------------
+tpath = '/home3/marcos.romero/phis-scq/angular-acceptance/'
+subs_dict = json.load(open(tpath+'input/time-angular-distribution.json','r') )
 
 
 
-test_data_h = np.array([
-  [-0.97683865, -0.6333215 ,  1.375432  ,  0.4200498 ],
-  [ 0.67043906, -0.05669873, -2.3950727 ,  2.5682745 ],
-  [ 0.2930908 , -0.88998055,  0.96165264,  2.1690416 ],
-  [ 0.05597173,  0.0991468 , -0.8175671 ,  1.423794  ],
-  [-0.02103739, -0.8631929 ,  2.837755  ,  2.3820534 ]]).astype(np.float64)
+# %% Get data (now a test file) ------------------------------------------------
 
+dpath = 'BsJpsiKK/Root/'+\
+'BsJpsiPhi_DG0_MC_2016_UpDown_MDST_20181101_Sim09b_tmva_cut58_sel_sw.root'
 
-test_out_h = np.zeros_like(test_data_h[:,0]).astype(np.float64)
-test_fk_h = np.zeros_like(10*[test_data_h[:,0]]).astype(np.float64).T
-
-test_data_d = pycl_array.to_device(queue,test_data_h)
-test_out_d  = pycl_array.to_device(queue,test_out_h)
-test_fk_d   = pycl_array.to_device(queue,test_fk_h)
+if platform.system() == "Darwin":
+    rfile = uproot.open('/Volumes/Santiago/Scratch03/'+dpath)["DecayTree"]
+elif platform.system() == "Linux":
+    rfile = uproot.open('/scratch03/marcos.romero/'+dpath)["DecayTree"]
 
 
 
+data = rfile.pandas.df(branches=['B_TRUETAU','truehelcosthetaK',
+                                'truehelcosthetaL', 'truehelphi','helcosthetaK',
+                                'helcosthetaL','helphi','time','sw'])
 
-# %% Prepare CUDA model --------------------------------------------------------
-module = cl.Program(context, open("/Users/marcos/phis-scq/opencl/AngularAcceptance.ocl","r").read() ).build()  # Create the OpenCL program
+data = data[(data['time']>0.3) & (data['time']<15) &
+            (1e3*data['B_TRUETAU']>0.3) & (1e3*data['B_TRUETAU']<15)]     # cuts
+
+vars_true_h = np.stack((
+                        data['truehelcosthetaK'].values,
+                        data['truehelcosthetaL'].values,
+                        data['truehelphi'].values,
+                        1e3*data['B_TRUETAU'].values
+                      ), axis=-1)
+
+vars_reco_h = np.stack((
+                        data['helcosthetaK'].values,
+                        data['helcosthetaL'].values,
+                        data['helphi'].values,
+                        data['time'].values
+                      ), axis=-1)
+
+vars_true_h = vars_true_h[:MAX_EVENTS,:]
+vars_reco_h = vars_reco_h[:MAX_EVENTS,:]
+
+sweights_h   = np.array(data['sw'].values)[:MAX_EVENTS]
+sweights_h  *= np.sum(sweights_h)/np.sum(sweights_h**2)
 
 
-def model(data, lkhd, amp, period, shift, decay):
-    cudaModel.shitModel(queue, data.shape, None, data.data, lkhd.data,
-                  np.float32(amp), np.float32(period), np.float32(shift),
-                  np.float32(decay), np.int16(len(data)) )
-    return lkhd.get()
+# Variables in gpuarray
+vars_true_d = gpuarray.to_gpu(vars_true_h).astype(np.float64)
+vars_reco_d = gpuarray.to_gpu(vars_reco_h).astype(np.float64)
+sweights_d  = gpuarray.to_gpu(sweights_h).astype(np.float64)
+
+
+# Result containers
+pdf_true_h  = np.zeros(vars_true_h.shape[0])
+fk_true_h   = np.zeros([vars_true_h.shape[0],10])
+pdf_reco_h  = np.zeros(vars_reco_h.shape[0])
+fk_reco_h   = np.zeros([vars_reco_h.shape[0],10])
+weights_h   = np.zeros([vars_reco_h.shape[0],10])
+
+pdf_true_d  = gpuarray.to_gpu(pdf_true_h).astype(np.float64)
+fk_true_d   = gpuarray.to_gpu(fk_true_h).astype(np.float64)
+pdf_reco_d  = gpuarray.to_gpu(pdf_reco_h).astype(np.float64)
+fk_reco_d   = gpuarray.to_gpu(fk_true_h).astype(np.float64)
+weights_d   = gpuarray.to_gpu(weights_h).astype(np.float64)
 
 
 
+# %% Run -----------------------------------------------------------------------
+
+# Get cuda kernels
+kernel_path = '/home3/marcos.romero/phis-scq/cuda/AngularAcceptance2.cu'
+AngularAcceptance = SourceModule(open(kernel_path,"r").read());
+cuDiffRate  = AngularAcceptance.get_function("pyDiffRate")
+cuFcoeffs   = AngularAcceptance.get_function("pyFcoeffs")
+cuAngularWeights   = AngularAcceptance.get_function("pyAngularWeights")
+
+# Define kernel wrappers
+def getCrossRate(vars,pdf,pars):
+  """
+  Look at kernel definition to see help
+  """
+  cuDiffRate( vars, pdf,
+              np.float64(pars["G"]), np.float64(pars["DG"]),
+              np.float64(pars["DM"]), np.float64(pars["CSP"]),
+              np.float64(pars["APlon"]), np.float64(pars["ASlon"]),
+              np.float64(pars["APpar"]), np.float64(pars["APper"]),
+              np.float64(pars["phisPlon"]), np.float64(pars["phisSlon"]),
+              np.float64(pars["phisPpar"]), np.float64(pars["phisPper"]),
+              np.float64(pars["deltaSlon"]), np.float64(pars["deltaPlon"]),
+              np.float64(pars["deltaPpar"]), np.float64(pars["deltaPper"]),
+              np.float64(pars["lPlon"]), np.float64(pars["lSlon"]),
+              np.float64(pars["lPpar"]), np.float64(pars["lPper"]),
+              np.int32(MAX_EVENTS),
+              block=(BLOCK_SIZE,1,1),
+              grid=(getGrid(pdf.shape[0], BLOCK_SIZE),1,1))
+  return pdf.get()
 
 
 
+def getAngularCoeffs(vars, fk):
+  """
+  Look at kernel definition to see help
+  """
+  cuFcoeffs(vars, fk, np.int32(MAX_EVENTS),
+            block=(BLOCK_SIZE,10,1),
+            grid=(getGrid(fk.shape[0], BLOCK_SIZE),1,1))
+  return fk.get()
 
 
 
+def getAngularWeights(vars_true,vars_reco,weights,pars):
+  """
+  getAngularWeights(vars_true,vars_reco,weights,pars):
 
+    In:
+            vars_true:  eventsx4 matrix that stores [cosK, cosK, hphi, time]
+                        variables in gpuarray -- true variables
+            vars_reco:  eventsx4 matrix that stores [cosK, cosK, hphi, time]
+                        variables in gpuarray -- reconstructed variables
+              weights:  1x10 gpuarray vector where angular weights will be
+                        stored
+                 pars:  python dict of all diff cross rate parameters, see below
+                        their (key) names
 
+    Out:
+                    0:  returns weights in host version, in a 1x10 np.array
 
-#%% run
+  Look at kernel definition to see more help
+  """
+  cuAngularWeights( vars_true,vars_reco, weights,
+              np.float64(pars["G"]), np.float64(pars["DG"]),
+              np.float64(pars["DM"]), np.float64(pars["CSP"]),
+              np.float64(pars["APlon"]), np.float64(pars["ASlon"]),
+              np.float64(pars["APpar"]), np.float64(pars["APper"]),
+              np.float64(pars["phisPlon"]), np.float64(pars["phisSlon"]),
+              np.float64(pars["phisPpar"]), np.float64(pars["phisPper"]),
+              np.float64(pars["deltaSlon"]), np.float64(pars["deltaPlon"]),
+              np.float64(pars["deltaPpar"]), np.float64(pars["deltaPper"]),
+              np.float64(pars["lPlon"]), np.float64(pars["lSlon"]),
+              np.float64(pars["lPpar"]), np.float64(pars["lPper"]),
+              np.int32(MAX_EVENTS),
+              block=(BLOCK_SIZE,1,1),
+              grid=(getGrid(vars_reco.shape[0], BLOCK_SIZE),1,1))
+  return weights.get()
 
-shit  = SourceModule(open('/home3/marcos.romero/JpsiKKAna/cuda/AngularAcceptance2.cu',"r").read());
-foo   = shit.get_function("pyDiffRate")
-bar   = shit.get_function("pyFcoeffs")
+#print(help(getAngularWeights))
 
-foo( test_data_d,  test_out_d,  subs_dict["G"],  subs_dict["DG"],  subs_dict["DM"],  subs_dict["CSP"],
-  subs_dict["APlon"],  subs_dict["ASlon"],  subs_dict["APpar"],  subs_dict["APper"],
-  subs_dict["phisPlon"],  subs_dict["phisSlon"],  subs_dict["phisPpar"],  subs_dict["phisPper"],
-  subs_dict["deltaSlon"],  subs_dict["deltaPlon"],  subs_dict["deltaPpar"],  subs_dict["deltaPper"],
-  subs_dict["lPlon"],  subs_dict["lSlon"],  subs_dict["lPpar"],  subs_dict["lPper"], np.int32(len(test_out_h)),
-  block=(5,1,1), grid=(1,1,1))
+# Compute and store in host
+fk_true_h  = getAngularCoeffs(vars_true_d, fk_true_d)
+pdf_true_h = getCrossRate(vars_true_d, pdf_true_d, subs_dict)
+fk_reco_h  = getAngularCoeffs(vars_reco_d, fk_reco_d)
+pdf_reco_h = getCrossRate(vars_reco_d, pdf_reco_d, subs_dict)
 
-bar( test_data_d,  test_fk_d, np.int32(len(test_data_d)), block=(5,10,1), grid=(1,1,1))
-test_fk_h  = test_fk_d.get()
-test_out_h = test_out_d.get()
+# Compute angular weights
+weights = fk_true_h/pdf_reco_h[:,None] #sweights_h[:,None]*
+weights = (weights.T/weights.T[0]).T                            # w[i,:]/w_[i,0]
 
-for item in (test_fk_h):
-  meh = ''
-  for subitem in item:
-    meh += "%+3.4f" % subitem + "\t"
+for k in range(0,len(weights)):
+  meh = 'event %d:\t' % k
+  w0 = weights[k,0]
+  for subitem in weights[k]:
+    meh += "%+3.4f \t" % (subitem)
   print(meh)
+
+# print(weights)
+weights_h = getAngularWeights(vars_true_d,vars_reco_d,weights_d,subs_dict)
 
 
 """
