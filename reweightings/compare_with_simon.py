a8d071a26afa41068a37df4ab8ed4a30d3bc2223
Marcos Romero <marcos.romero.lamas@cern.ch>
2020-07-21 14:27:52 +0200

    Script to compare v0r1 reweightings with Simon ones

--- reweightings/compare_with_simon.py
+++ reweightings/compare_with_simon.py
@@ -1,7 +1,7 @@
 # The main purpose of this file is to compare old Run2 weightings done by
 # Simon with the ones phis-scq produces.
 
-%matplotlib inline
+#%matplotlib inline
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,11 +14,11 @@ from ipanema import histogram
 # Copy of old tuples is at:
 old_path = '/scratch03/marcos.romero/phisRun2/cooked_test_files/'
 # and new ones are at:
-new_path = '/scratch17/marcos.romero/phis_samples/v0r2'
+new_path = '/scratch17/marcos.romero/phis_samples/'
 
 # Create some paths
-os.makedirs('output/reweightings/2016', exist_ok=True )
-os.makedirs('output/reweightings/2015', exist_ok=True )
+os.makedirs('output_new/figures/reweightings/2016', exist_ok=True )
+os.makedirs('output_new/figures/reweightings/2015', exist_ok=True )
 
 
 
@@ -29,14 +29,15 @@ old = {}; new = {}
 for y in ['2015','2016']:
   old[y] = {}; t_o_p = os.path.join(old_path,y)
   new[y] = {}; t_n_p = os.path.join(new_path,y)
-  for m in ['Bd2JpsiKstar', 'MC_Bs2JpsiPhi_dG0', #'MC_Bs2JpsiPhi',
+  for m in ['Bd2JpsiKstar', 'MC_Bs2JpsiPhi_dG0', 'MC_Bs2JpsiPhi',
             'Bs2JpsiPhi', 'MC_Bd2JpsiKstar']:
     old[y][m] = Sample.from_root(os.path.join(t_o_p,m,'test_kinWeight.root'))
-    new[y][m] = Sample.from_root(os.path.join(t_n_p,m,'test.root'))
+    new[y][m] = Sample.from_root(os.path.join(t_n_p,m,'v0r0.root'))
+    os.makedirs('output_new/figures/reweightings/'+y+f'/{m}', exist_ok=True )
 
 
 
-for year in  [2015, 2016]:
+for year in  [2015,2016]:
   #%% polWeight
   try:
     fig, axplot, axpull = ipanema.plotting.axes_plotpull()
@@ -49,9 +50,9 @@ for year in  [2015, 2016]:
     axplot.set_title(f"max(Heidelberg - Santiago) = {np.amax(old[f'{year}'][f'MC_Bs2JpsiPhi'].df['polWeight']-new[f'{year}'][f'MC_Bs2JpsiPhi'].df['polWeight']):0.3g}")
     axplot.set_ylabel('Events')
     axplot.legend()
-    fig.savefig(f'output/reweightings/{year}/MC_Bs2JpsiPhi_pdfWeight.pdf')
+    fig.savefig(f'output_new/figures/reweightings/{year}/MC_Bs2JpsiPhi/v0r0_polWeightComparison.pdf')
   except:
-    print(f'Failed in output/reweightings/{year}/MC_Bs2JpsiPhi_pdfWeight.pdf')
+    print(f'Failed in output_new/figures/reweightings/{year}/MC_Bs2JpsiPhi/v0r0_polWeightComparison.pdf')
   try:
     fig, axplot, axpull = ipanema.plotting.axes_plotpull()
     o, n = histogram.compare_hist([old[f'{year}'][f'MC_Bs2JpsiPhi_dG0'].df['polWeight'],new[f'{year}'][f'MC_Bs2JpsiPhi_dG0'].df['polWeight']], bins=4,range=(0,2))
@@ -63,9 +64,9 @@ for year in  [2015, 2016]:
     axplot.set_title(f"max(Heidelberg - Santiago) = {np.amax(old[f'{year}'][f'MC_Bs2JpsiPhi_dG0'].df['polWeight']-new[f'{year}'][f'MC_Bs2JpsiPhi_dG0'].df['polWeight']):0.3g}")
     axplot.set_ylabel('Events')
     axplot.legend()
-    fig.savefig(f'output/reweightings/{year}/MC_Bs2JpsiPhi_dG0_pdfWeight.pdf')
+    fig.savefig(f'output_new/figures/reweightings/{year}/MC_Bs2JpsiPhi_dG0/v0r0_polWeightComparison.pdf')
   except:
-    print(f'Failed in output/reweightings/{year}/MC_Bs2JpsiPhi_pdfWeight.pdf')
+    print(f'Failed in output_new/figures/reweightings/{year}/MC_Bs2JpsiPhi/v0r0_polWeightComparison.pdf')
   try:
     fig, axplot, axpull = ipanema.plotting.axes_plotpull()
     o, n = histogram.compare_hist([old[f'{year}'][f'MC_Bd2JpsiKstar'].df['polWeight'],new[f'{year}'][f'MC_Bd2JpsiKstar'].df['polWeight']], bins=4,range=(0,2))
@@ -77,9 +78,9 @@ for year in  [2015, 2016]:
     axplot.set_title(f"max(Heidelberg - Santiago) = {np.amax(old[f'{year}'][f'MC_Bd2JpsiKstar'].df['polWeight']-new[f'{year}'][f'MC_Bd2JpsiKstar'].df['polWeight']):0.3g}")
     axplot.set_ylabel('Events')
     axplot.legend()
-    fig.savefig(f'output/reweightings/{year}/MC_Bd2JpsiKstar_pdfWeight.pdf')
+    fig.savefig(f'output_new/figures/reweightings/{year}/MC_Bd2JpsiKstar/v0r0_polWeightComparison.pdf')
   except:
-    print(f'Failed in output/reweightings/{year}/MC_Bs2JpsiPhi_pdfWeight.pdf')
+    print(f'Failed in output_new/figures/reweightings/{year}/MC_Bs2JpsiPhi/v0r0_polWeightComparison.pdf')
 
 
   #%% pdfWeight
@@ -93,7 +94,7 @@ for year in  [2015, 2016]:
   axplot.set_title(f"max(Heidelberg - Santiago) = {np.amax(old[f'{year}']['MC_Bs2JpsiPhi_dG0'].df['pdfWeight']-new[f'{year}']['MC_Bs2JpsiPhi_dG0'].df['pdfWeight']):0.3g}")
   axplot.set_ylabel('Events')
   axplot.legend()
-  fig.savefig(f'output/reweightings/{year}/MC_Bs2JpsiPhi_dG0_pdfWeight.pdf')
+  fig.savefig(f'output_new/figures/reweightings/{year}/MC_Bs2JpsiPhi_dG0/v0r0_pdfWeightComparison.pdf')
 
   fig, axplot, axpull = ipanema.plotting.axes_plotpull()
   o, n = histogram.compare_hist([old[f'{year}']['MC_Bd2JpsiKstar'].df['pdfWeight'],new[f'{year}']['MC_Bd2JpsiKstar'].df['pdfWeight']],bins=50,range=(0,2))
@@ -105,7 +106,7 @@ for year in  [2015, 2016]:
   axplot.set_title(f"max(Heidelberg - Santiago) = {np.amax(old[f'{year}']['MC_Bd2JpsiKstar'].df['pdfWeight']-new[f'{year}']['MC_Bd2JpsiKstar'].df['pdfWeight']):0.3g}")
   axplot.set_ylabel('Events')
   axplot.legend()
-  fig.savefig(f'output/reweightings/{year}/MC_Bd2JpsiKstar_pdfWeight.pdf')
+  fig.savefig(f'output_new/figures/reweightings/{year}/MC_Bd2JpsiKstar/v0r0_pdfWeightComparison.pdf')
 
   #%% kinWeight
   fig, axplot, axpull = ipanema.plotting.axes_plotpull()
@@ -118,7 +119,7 @@ for year in  [2015, 2016]:
   axplot.set_title(f"max(Heidelberg - Santiago) = {np.amax(old[f'{year}']['MC_Bs2JpsiPhi_dG0'].df['kinWeight']-new[f'{year}']['MC_Bs2JpsiPhi_dG0'].df['kinWeight']):0.3g}")
   axplot.set_ylabel('Events')
   axplot.legend()
-  fig.savefig(f'output/reweightings/{year}/MC_Bs2JpsiPhi_dG0_kinWeight.pdf')
+  fig.savefig(f'output_new/figures/reweightings/{year}/MC_Bs2JpsiPhi_dG0/v0r0_kinWeightComparison.pdf')
 
   fig, axplot, axpull = ipanema.plotting.axes_plotpull()
   o, n = histogram.compare_hist([old[f'{year}']['MC_Bd2JpsiKstar'].df['kinWeight'],new[f'{year}']['MC_Bd2JpsiKstar'].df['kinWeight']],bins=50,range=(0,2))
@@ -130,7 +131,7 @@ for year in  [2015, 2016]:
   axplot.set_title(f"max(Heidelberg - Santiago) = {np.amax(old[f'{year}']['MC_Bd2JpsiKstar'].df['kinWeight']-new[f'{year}']['MC_Bd2JpsiKstar'].df['kinWeight']):0.3g}")
   axplot.set_ylabel('Events')
   axplot.legend()
-  fig.savefig(f'output/reweightings/{year}/MC_Bd2JpsiKstar_kinWeight.pdf')
+  fig.savefig(f'output_new/figures/reweightings/{year}/MC_Bd2JpsiKstar/v0r0_kinWeightComparison.pdf')
 
   fig, axplot, axpull = ipanema.plotting.axes_plotpull()
   o, n = histogram.compare_hist([old[f'{year}'][f'Bd2JpsiKstar'].df['kinWeight'],new[f'{year}'][f'Bd2JpsiKstar'].df['kinWeight']])
@@ -142,4 +143,4 @@ for year in  [2015, 2016]:
   axplot.set_title(f"max(Heidelberg - Santiago) = {np.amax(old[f'{year}'][f'Bd2JpsiKstar'].df['kinWeight']-new[f'{year}'][f'Bd2JpsiKstar'].df['kinWeight']):0.3g}")
   axplot.set_ylabel('Events')
   axplot.legend()
-  fig.savefig(f'output/reweightings/{year}/Bd2JpsiKstar_kinWeight.pdf')
+  fig.savefig(f'output_new/figures/reweightings/{year}/Bd2JpsiKstar/v0r0_kinWeightComparison.pdf')
