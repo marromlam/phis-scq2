596bf282d38a3ba1d1dbc80e03e7046464396891
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2020-10-02 16:51:40 +0200

    use xrootd instead of cp

--- samples/new_downloader.py
+++ samples/new_downloader.py
@@ -2,9 +2,11 @@ import uproot
 import argparse
 import os
 import pandas as pd
+import hjson
+import numpy as np
 
-
-binned_vars = ['eta', 'pt', 'sigmat']
+binned_vars = {'eta':'B_ETA', 'pt':'B_PT', 'sigmat':'sigmat'}
+bin_vars = hjson.load(open('config.json'))['binned_variables_cuts']
 
 def argument_parser():
   parser = argparse.ArgumentParser(description='Sync tuples.')
@@ -50,18 +52,18 @@ if __name__ == "__main__":
   print(f"Downloading {m}_{y}_selected_bdt_sw_{v}.root")
   eos_path  = f'/eos/lhcb/wg/B2CC/Bs2JpsiPhi-FullRun2'
   eos_path += f'/{v}/{m}/{y}/{m}_{y}_selected_bdt_sw_{v}.root'
-  status = os.system(f"""scp lxplus:{eos_path} {scq_path}""")
+  status = os.system(f"""xrdcp root://eoslhcb.cern.ch/{eos_path} {scq_path}""")
   print(status)
   if status==0:
     all_files.append([f"{m}_{y}_selected_bdt_sw_{v}.root",None])
   else:
     print(f"    File {m}_{y}_selected_bdt_sw_{v}.root does not exist on server.")
 
-  for var in binned_vars:
+  for var in binned_vars.keys():
     print(f"Downloading {m}_{y}_selected_bdt_sw_{var}_{v}.root")
     eos_path  = f'/eos/lhcb/wg/B2CC/Bs2JpsiPhi-FullRun2'
     eos_path += f'/{v}/fit_check/{m}/{y}/{m}_{y}_selected_bdt_sw_{var}_{v}.root'
-    status = os.system(f"""scp -r lxplus:{eos_path} {scq_path}""")
+    status = os.system(f"""xrdcp -r root://eoslhcb.cern.ch/{eos_path} {scq_path}""")
     print(status)
     if status==0:
       all_files.append([f"{m}_{y}_selected_bdt_sw_{var}_{v}.root",f"{var}"])
@@ -79,6 +81,14 @@ if __name__ == "__main__":
 
   # concatenate all columns
   result = pd.concat(all_dfs, axis=1)
+  for var in binned_vars.keys():
+    if not f'sw_{var}' in list(result.keys()):
+      sw = np.zeros_like(result[f'sw'])
+      for cut in bin_vars[var]:
+        pos = result.eval(cut.replace(var,binned_vars[var]))
+        this_sw = result.eval(f'sw*({cut.replace(var,binned_vars[var])})')
+        sw = np.where(pos, this_sw * ( sum(this_sw)/sum(this_sw*this_sw) ),sw)
+      result[f'sw_{var}'] = result[f'sw']#sw
 
   # write
   print(f"\nStarting to write {os.path.basename(args['output'])} file.")
