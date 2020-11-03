9a4e7a46bed096f707093a0587d2c50a1621971a
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2020-11-03 12:29:14 +0100

    [ci-skip] now coeffs for time acceptacen are differently handled (more general)

--- analysis/time_acceptance/fcn_functions.py
+++ analysis/time_acceptance/fcn_functions.py
@@ -1,29 +1,32 @@
-# -*- coding: utf-8 -*-
+DESCRIPTION = """
+    This file contains 3 fcn functions to be minimized under ipanema3 framework
+    those functions are, actually functions of badjanak kernels.
+"""
 
-__author__ = ['Marcos Romero']
+__author__ = ['Marcos Romero Lamas']
 __email__ = ['mromerol@cern.ch']
+__all__ = ['splinexerf', 'saxsbxscxerf', 'splinexerfconstr']
 
-__all__ = ['splinexerf','saxsbxscxerf']
 
-
-from ipanema import ristra
+from ipanema import ristra, Parameters
 import numpy as np
 import badjanak
 
-from utils.plot import mode_tex
-from utils.strings import cuts_and
+def splinexerf(params, data, weight = None, prob = None):
+  # do lists of coeffs
+  c = [ k.value for k in params.fetch('(a|c|b)([1-9])?([0-9])(u|b)').values() ]
 
+  mu = params[params.find('(mu).*')[0]].value
+  sigma = params[params.find('(sigma).*')[0]].value
+  gamma = params[params.find('(gamma).*')[0]].value
 
-
-def splinexerf(parameters, data, weight = None, prob = None):
-  pars_dict = list(parameters.valuesdict().values())
   if not prob: # for ploting, mainly
     data = ristra.allocate(data)
     prob = ristra.allocate(np.zeros_like(data.get()))
-    badjanak.splinexerf(data, prob, *pars_dict)
+    badjanak.splinexerf(data, prob, coeffs=c, mu=mu, sigma=sigma, gamma=gamma)
     return prob.get()
   else:
-    badjanak.splinexerf(data, prob, *pars_dict)
+    badjanak.splinexerf(data, prob, coeffs=c, mu=mu, sigma=sigma, gamma=gamma);
     if weight is not None:
       result = (ristra.log(prob)*weight).get()
     else:
@@ -32,14 +35,35 @@ def splinexerf(parameters, data, weight = None, prob = None):
 
 
 def saxsbxscxerf(params, data, weight=False, prob=None):
-  pars = params.valuesdict()
-  if not prob:
-    samples = list( map(ristra.allocate,data) )
-    prob = list( map(ristra.zeros_like,samples) )
-    badjanak.saxsbxscxerf(*samples, *prob, **pars)
+  # do lists of coeffs
+  a = [ k.value for k in params.fetch('(a|aA|bA)([1-9])?([0-9])(u|b)').values() ]
+  b = [ k.value for k in params.fetch('(b|aB|bB)([1-9])?([0-9])(u|b)').values() ]
+  c = [ k.value for k in params.fetch('(c)(A|B)?([1-9])?([0-9])(u|b)').values() ]
+
+  # get mu, sigma and gamma for each data set in the simultaneous fit
+  mu_a = params[params.find('(mu)_(a|Aa|Ab)')[0] ].value
+  mu_b = params[params.find('(mu)_(b|Ba|Bb)')[0] ].value
+  mu_c = params[params.find('(mu)_(A|B)?(c)')[0] ].value
+  sigma_a = params[params.find('(sigma)_(a|Aa|Ab)')[0] ].value
+  sigma_b = params[params.find('(sigma)_(b|Ba|Bb)')[0] ].value
+  sigma_c = params[params.find('(sigma)_(A|B)?(c)')[0] ].value
+  gamma_a = params[params.find('(gamma)_(a|Aa|Ab)')[0] ].value
+  gamma_b = params[params.find('(gamma)_(b|Ba|Bb)')[0] ].value
+  gamma_c = params[params.find('(gamma)_(A|B)?(c)')[0] ].value
+
+  if not prob: # for ploting, mainly
+    data = list( map(ristra.allocate, data) )
+    prob = list( map(ristra.zeros_like, data) )
+    badjanak.saxsbxscxerf(*data, *prob, coeffs_a=a, coeffs_b=b, coeffs_c=c,
+                          mu_a=mu_a, mu_b=mu_b, mu_c=mu_c,
+                          sigma_a=sigma_a, sigma_b=sigma_b, sigma_c=sigma_c,
+                          gamma_a=gamma_a, gamma_b=gamma_b, gamma_c=gamma_c)
     return [ p.get() for p in prob ]
   else:
-    badjanak.saxsbxscxerf(*data, *prob, **pars)
+    badjanak.saxsbxscxerf(*data, *prob, coeffs_a=a, coeffs_b=b, coeffs_c=c,
+                          mu_a=mu_a, mu_b=mu_b, mu_c=mu_c,
+                          sigma_a=sigma_a, sigma_b=sigma_b, sigma_c=sigma_c,
+                          gamma_a=gamma_a, gamma_b=gamma_b, gamma_c=gamma_c)
     if weight:
       result  = np.concatenate(( (ristra.log(prob[0])*weight[0]).get(),
                                  (ristra.log(prob[1])*weight[1]).get(),
