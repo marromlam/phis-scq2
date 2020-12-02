3b35f81428b3825aecb2cb45f3897e97e6415382
Ramon Angel Ruiz Fernandez <ramon.angel.ruiz.fernandez@cern.ch>
2020-12-02 13:57:55 +0100

    add pdf Bd

---
+++ analysis/badjanak/DifferentialCrossRateIntegratedTime.cu
@@ -0,0 +1,88 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//                         DIFFERENTIAL CROSS RATE                            //
+//                                                                            //
+//   Created: 2020-06-25                                                      //
+//    Author: Marcos Romero Lamas (mromerol@cern.ch)                          //
+//                                                                            //
+//    This file is part of phis-scq packages, Santiago's framework for the    //
+//                     phi_s analysis in Bs -> Jpsi K+ K-                     //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Functions ///////////////////////////////////////////////////////////////////
+
+//Ramon es mejor pasar el data igual para no confundirse en otras partes del codigo
+
+WITHIN_KERNEL
+ftype getDiffRateBd( ftype *data,
+                    ftype CSP,
+                    ftype ASlon, ftype APlon, ftype APpar, ftype APper,
+                    ftype dSlon, ftype dPlon, ftype dPpar, ftype dPper,
+                    // Angular acceptance
+                    GLOBAL_MEM  ftype *angular_weights,
+                    int USE_FK
+                  )
+{
+
+
+  // Variables -----------------------------------------------------------------
+  //     Make sure that the input it's in this order.
+  //     lalala
+  ftype cosK       = data[0];                      // Time-angular distribution
+  ftype cosL       = data[1];
+  ftype hphi       = data[2];                            // Time resolution
+  ftype qOS        = data[5];                                      // Tagging
+
+
+
+  // Flavor tagging ------------------------------------------------------------
+  ftype id;
+  id = qOS/fabs(qOS);
+
+  // Compute per event pdf -----------------------------------------------------
+  ftype fk, ak;
+  ftype pdfB = 0.0;
+  ftype norm = 0.0;
+
+  for(int k = 1; k <= 10; k++)
+  {
+    if (USE_FK)
+    {
+      fk = ( 9.0/(16.0*M_PI) )*getF(cosK,cosL,hphi,k);
+    }
+    else
+    {
+      fk = TRISTAN[k-1];
+    }
+    ak = getAbd(ASlon, APlon, APpar, APper, dSlon, dPpar, dPpar, dPper, CSP, k);
+    //norm += angular_weights[k-1]*ak;
+    norm += TRISTAN[k-1]*ak;
+    if ( (k==4) || (k==6)  || (k==9) )
+    {
+      pdfB += id*fk*ak;
+    }
+    else
+    {
+      pdfB += fk*ak;
+    }
+  }
+
+
+  return pdfB/norm;
+}
