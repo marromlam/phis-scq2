c127bd8c09f2e92df4adbf4b6e98582caa3d1c53
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2020-12-10 13:53:49 +0100

    add Bu samples

--- analysis/utils/helpers.py
+++ analysis/utils/helpers.py
@@ -33,6 +33,7 @@ import numpy as np
 
 def timeacc_guesser(timeacc):
   # Check if the tuple will be modified
+  #pattern = r'\A(single|simul|lifeBd|lifeBu)(1[0-2]|[3-9]knots)?(Noncorr)?(deltat|alpha|mKstar)?(Minos|BFGS|LBFGSB|CG|Nelder|EMCEE)?\Z'
   pattern = r'\A(single|simul|lifeBd|lifeBu)(1[0-2]|[3-9])?(Noncorr)?(deltat|alpha|mKstar)?(Minos|BFGS|LBFGSB|CG|Nelder|EMCEE)?\Z'
   p = re.compile(pattern)
   try:
@@ -40,6 +41,7 @@ def timeacc_guesser(timeacc):
     corr = False if corr=='Noncorr' else True
     mini = mini.lower() if mini else 'minuit'
     knots = int(knots) if knots else 6
+    #knots = int(knots[0]) if knots else 6
     return acc, knots, corr, lifecut, mini
   except:
     raise ValueError(f'Cannot interpret {timeacc} as a timeacc modifier')
@@ -115,7 +117,7 @@ def tuples(wcs, version=False, year=None, mode=None, weight=None):
       elif m == 'Bd2JpsiKstar':
         m = 'Bs2JpsiPhi'
     elif mode in ('Bs2JpsiPhi','MC_Bs2JpsiPhi_dG0','MC_Bs2JpsiPhi',
-                  'Bd2JpsiKstar', 'MC_Bd2JpsiKstar'):
+                  'Bd2JpsiKstar', 'MC_Bd2JpsiKstar', 'Bu2JpsiKplus', 'MC_Bu2JpsiKplus'):
       m = mode
 
 
@@ -123,6 +125,12 @@ def tuples(wcs, version=False, year=None, mode=None, weight=None):
   if m == 'Bs2JpsiPhi':
     if weight:
       weight = 'sWeight'
+  elif m == 'Bu2JpsiKplus':
+    if weight:
+      weight = 'sWeight'
+  elif m == 'MC_Bu2JpsiKplus':
+    if weight:
+      weight = 'polWeight'
   elif m == 'Bd2JpsiKstar':
     if weight:
       if weight != 'kinWeight':
