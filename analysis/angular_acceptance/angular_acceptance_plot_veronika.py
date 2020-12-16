e049f5733968d41f94482a2d4f987eefd1a3929a
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2020-12-16 16:29:32 +0100

    add projection full pdf

--- analysis/angular_acceptance/angular_acceptance_plot_veronika.py
+++ analysis/angular_acceptance/angular_acceptance_plot_veronika.py
@@ -39,6 +39,31 @@ def angeff_plot(angacc, cosK, cosL, hphi, project=None):
 
 
 
+
+#Parameters.load(f'output/params/angular_acceptance//Bs2JpsiPhi/v0r5_naive_{trigger}.json')
+
+
+
+
+def expected_value(angacc, cosK, cosL, hphi, project=None):
+  eff = ristra.zeros_like(cosK)
+  try:
+    _angacc = ristra.allocate(np.array(angacc))
+  except:
+    _angacc = ristra.allocate(np.array([a.n for a in angacc]))
+  badjanak.__KERNELS__.plot_moments(_angacc, eff, cosK, cosL, hphi, global_size=(eff.shape[0],))
+  n = round(eff.shape[0]**(1/3))
+  res = ristra.get(eff).reshape(n,n,n)
+  if project==1:
+    return np.sum(res,(1,0))
+  if project==2:
+    return np.sum(res,(1,2))
+  if project==3:
+    return np.sum(res,(2,0))
+  return res
+
+
+
 N = 100 # number of points to plot
 cosK = np.linspace(-1,1,N)
 cosL = np.linspace(-1,1,N)
@@ -60,11 +85,11 @@ for var in ['cosK', 'cosL', 'hphi']:
     proj = 2; bounds = (-np.pi,np.pi); tex = r'\phi_h'; x = hphi
   else:
     print('huge error')
-  for year in [2015,2016,2017,2018]:
+  for year in [2015,2016]:#,2017,2018]:
     for trigger in ['biased', 'unbiased']:
       fig, ax = plt.subplots()
       # first plot the baseline and its confidence band (1sigma)
-      angacc = Parameters.load(f'output/params/angular_acceptance/{year}/MC_Bs2JpsiPhi_dG0/v0r5_naive_{trigger}.json')
+      angacc = Parameters.load(f'output/params/angular_acceptance/{year}/Bs2JpsiPhi/v0r1_naive_{trigger}.json')
       eff = angeff_plot(angacc, cosKd, cosLd, hphid, proj)
       norm = np.trapz(eff, x)
       langacc = [p.uvalue for p in angacc.values()]
@@ -73,8 +98,8 @@ for var in ['cosK', 'cosL', 'hphi']:
       ax.fill_between(x, yl/norm,yh/norm, alpha=0.5)
       ax.plot(x, eff/norm, label='nominal')
       # then run over all time-binned angular acceptances
-      for i in range(1,7):
-        angacc = Parameters.load(f'output/params/angular_acceptance/{year}/MC_Bs2JpsiPhi_dG0/v0r5_naiveTime{i}_{trigger}.json')
+      for i in range(1,4):
+        angacc = Parameters.load(f'output/params/angular_acceptance/{year}/Bs2JpsiPhi/v0r1_naive2knots{i}_{trigger}.json')
         eff = angeff_plot(angacc, cosKd, cosLd, hphid, proj)
         eff /= np.trapz(eff, x)
         ax.plot(x,eff, '-.',label=f'$t$ bin = {i}')
@@ -82,4 +107,4 @@ for var in ['cosK', 'cosL', 'hphi']:
       ax.legend()
       #ax.set_ylim(0.9,1.25)
       ax.set_title(f"{year} {trigger}")
-      fig.savefig(f'output/figures/angular_acceptance/{year}/Bs2JpsiPhi/v0r5_naiveTime{var.title()}_{trigger}.pdf')
+      fig.savefig(f'output/figures/angular_acceptance/{year}/Bs2JpsiPhi/v0r1_naive2knots{var.title()}_{trigger}.pdf')
