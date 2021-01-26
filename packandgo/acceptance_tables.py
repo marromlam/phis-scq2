d820f36b2aeeb30a0940a4a17f59088dd6149bac
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2021-01-26 17:17:34 +0100

    Add packandgo scripts for acceptances and Snakefile

--- packandgo/acceptance_tables.py
+++ packandgo/acceptance_tables.py
@@ -2,12 +2,18 @@ DESCRIPTION = """
   hey there
 """
 
+__author__ = ['Marcos Romero Lamas']
+__email__ = ['mromerol@cern.ch']
+__all__ = ['dump_joint_acceptance']
+
 from analysis.utils.plot import mode_tex
 from ipanema import Parameters
 import argparse
 
 def dump_joint_acceptance(pu, pb, years, caption=""):
     texcode  = f"\\begin{{table}}[H]\n"
+    texcode += f"\\centering\n"
+    texcode += f"\\caption{{{caption}}}\n"
     texcode += f"\\begin{{tabular}}{{{'c'*(len(years)+1)}}}\n"
     texcode += "\\toprule\n"
     texcode += " & ".join([f"  {'':>5}  "]+[f"$ {y:>20} $" for y in years])
@@ -31,7 +37,6 @@ def dump_joint_acceptance(pu, pb, years, caption=""):
     texcode += ' \\\\\n\\midrule\n'+' \\\\ \n'.join(table)
     texcode += ' \\\\\n\\bottomrule\n'
     texcode += f"\\end{{tabular}}\n"
-    texcode += f"\\caption{{{caption}}}\n"
     texcode += f"\\end{{table}}\n"
     return texcode
 
@@ -52,7 +57,7 @@ if __name__ == '__main__':
   parser.add_argument('--timeacc', help='Time Acceptance Flag')
   parser.add_argument('--angacc', default=False, help='Angular Acceptance Flag')
   args = vars(parser.parse_args())
-  
+
   # split inputs
   bpp = args['biased'].split(',')
   upp = args['unbiased'].split(',')
@@ -62,17 +67,17 @@ if __name__ == '__main__':
   acc = args['timeacc']
   if args['angacc']:
     acc = f"{args['angacc']}--{acc}"
-  
+
   # load parameters
   pu = [Parameters.load(ip) for ip in upp]
   pb = [Parameters.load(ip) for ip in bpp]
-  
+
   # create a caption
   if args['angacc']:
     caption = f"Angular acceptance [{acc}] for ${'+'.join(years)}$."
   else:
     caption = f"Time acceptance [{acc}] fitted against ${mode_tex(m)}$ for ${'+'.join(years)}$."
-  
+
   # tabule
   table = dump_joint_acceptance(pu, pb, years, caption=caption)
   print(table)
