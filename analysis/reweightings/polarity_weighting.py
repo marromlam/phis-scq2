e8c99d2f4aa8825361b7b8b205c556224599ae5a
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2021-06-01 14:31:55 +0000

    golden candidate for polarity_weighting

--- analysis/reweightings/polarity_weighting.py
+++ analysis/reweightings/polarity_weighting.py
@@ -1,68 +1,55 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+DESCRIPTION = """
+  Compute polarity weight between data and MC.
+"""
 
-# %% Modules -------------------------------------------------------------------
+
+__author__ = ['Marcos Romero Lamas']
+__email__ = ['mromerol@cern.ch']
+__all__ = ['polarity_weighting']
+
+
+# Modules {{{
+
 import argparse
 import numpy as np
 import uproot3 as uproot
-from shutil import copyfile
-import os
+from utils.strings import printsec
 
-ROOT_PANDAS = True
+ROOT_PANDAS = False
 if ROOT_PANDAS:
-  import root_pandas
+  from shutil import copyfile
   import root_numpy
+  # import root_pandas
+
+# }}}
 
 
-# %% polarity_weighting --------------------------------------------------------
+# Polarity Weighting {{{
 
-def argument_parser():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--original-file',
-                      help='File to correct')
-  parser.add_argument('--original-treename', default='DecayTree',
-                      help='Name of the original tree')
-  parser.add_argument('--target-file',
-                      help='File to reweight to')
-  parser.add_argument('--target-treename', default='DecayTree',
-                      help='Name of the target tree')
-  parser.add_argument('--output-file',
-                      help='File to store the ntuple with weights')
-  return parser
-
-
-
-def polarity_weighting(original_file, original_treename,
-                       target_file, target_treename,
-                       output_file, verbose=False):
-  # Get data
-  odf = uproot.open(original_file)[original_treename].pandas.df(flatten=None)
+def polarity_weighting(original_polarity, target_polarity, verbose=False):
+  # load polarity
   original_polarity = odf["Polarity"].values
-  #original_polarity = odf["magnet"].values
-  tdf = uproot.open(target_file)[target_treename].pandas.df(branches="Polarity")
   target_polarity = tdf["Polarity"].values
-  #tdf = uproot.open(targ#et_file)[target_treename].pandas.df(branches="magnet")
-  #target_polarity = tdf["magnet"].values
-  print('DataFrames are ready')
-  print(odf)
 
-  # Cook weights
+  # cook weights
   original_mean = np.mean(original_polarity)
-  target_mean   = np.mean(target_polarity)
+  target_mean = np.mean(target_polarity)
   original_down = np.sum(np.where(original_polarity<0,1,0))
-  original_up   = np.sum(np.where(original_polarity>0,1,0))
+  original_up = np.sum(np.where(original_polarity>0,1,0))
 
-  weight_up   = 1
-  weight_down = ( (1+original_mean) * (1-target_mean) ) /\
-                ( (1-original_mean) * (1+target_mean) )
+  weight_up = 1
+  weight_down = ( (1 + original_mean) * (1 - target_mean) ) /\
+                ( (1 - original_mean) * (1 + target_mean) )
 
   weight_scale = (original_down + original_up) /\
                  (weight_down*original_down + weight_up*original_up)
-  weight_up   *= weight_scale
+  weight_up *= weight_scale
   weight_down *= weight_scale
 
   polWeight = np.where(original_polarity>0, weight_up, weight_down)
+  odf['polWeight'] = polWeight
 
+  # TO BE REMOVED
   # bee = uproot.open('/scratch03/marcos.romero/phisRun2/original_test_files/2016/BsJpsiPhi_DG0_MC_2016_UpDown_MDST_20181101_Sim09b_tmva_cut58_sel_sw_PolWeight.root')['PolWeight']
   # bee = uproot.open('/scratch03/marcos.romero/phisRun2/original_test_files/2016/BdJpsiKstar_MC_2016_UpDown_CombDSTLDSTMDST_20181101_CombSim09bSim09c_tmva_cut58_sel_sw_trigCat_PolWeight.root')['PolWeight']
   #
@@ -70,33 +57,54 @@ def polarity_weighting(original_file, original_treename,
   # print(polWeight[0],polWeight[-1])
   # print(f"CHECK: {np.amax( polWeight - bee.array('PolWeight') )}")
 
-  odf['polWeight'] = polWeight
-  print('polWeight was succesfully calculated')
+  return odf
+
+# }}}
+
+
+# CMDLINE interface {{{
+
+if __name__ == '__main__':
+
+  # parse command line arguments
+  p = argparse.ArgumentParser(DESCRIPTION)
+  p.add_argument('--original-file',
+                 help='File to correct')
+  p.add_argument('--original-treename', default='DecayTree',
+                 help='Name of the original tree')
+  p.add_argument('--target-file',
+                 help='File to reweight to')
+  p.add_argument('--target-treename', default='DecayTree',
+                 help='Name of the target tree')
+  p.add_argument('--output-file',
+                 help='File to store the ntuple with weights')
+  args = vars(p.parse_args())
+
+  # load tuples
+  ofile = args['original_file']; tfile = args['target_file']
+  otree = args['original_treename']; ttree = args['target_treename']
+  odf = uproot.open(ofile)[otree].pandas.df(flatten=None)
+  tdf = uproot.open(tfile)[ttree].pandas.df(branches="Polarity")
+
+  # run 
+  printsec("Polarity weighting")
+  print(tdf)
+  odf = polarity_weighting(odf, tdf, verbose=True)
   print(odf)
 
   # Save weights to file
-  #    It would be nice that uproot.update worked, but it is not yet avaliable
-  #    and the function is a placeholder only according to the author. So, at
-  #    moment it is needed to load the whole df and write a new one :(.
-  #    This produces some problems with large files, so we are using root_pandas
-  #    to store files whilst uproot can't
   if ROOT_PANDAS:
-    copyfile(original_file, output_file)
-    var = np.array(polWeight, dtype=[('polWeight', np.float64)])
-    root_numpy.array2root(var, output_file, original_treename, mode='update')
+    copyfile(ofile, tfile)
+    polWeight = np.array(odf['polWeight'].values, dtype=[('polWeight', np.float64)])
+    root_numpy.array2root(polWeight, ofile, otree, mode='update')
+    # root_pandas.to_root(odf, args['output_file'], key=otree)
   else:
-    f = uproot.recreate(output_file)
-    f[original_treename] = uproot.newtree({var:'float64' for var in odf})
-    f[original_treename].extend(odf.to_dict(orient='list'))
-    f.close()
+    with uproot.recreate(args['output_file']) as rf:
+      rf[otree] = uproot.newtree({var:'float64' for var in odf})
+      rf[otree].extend(odf.to_dict(orient='list'))
   print('polWeight was succesfully written.')
 
-  return polWeight
+# }}}
 
 
-if __name__ == '__main__':
-  parser = argument_parser()
-  args = parser.parse_args()
-  print(f"\n{80*'='}\nPolarity weighting\n{80*'='}\n")
-  polarity_weighting(**vars(args), verbose=True)
-  print(f"\n")
+# vim: foldmethod=marker
