822aebc92eaa1b0a895e4ed40739a8bf2ee1341e
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2021-06-23 00:46:55 +0000

    add more jsons to config

--- config/__init__.py
+++ config/__init__.py
@@ -15,10 +15,47 @@ timeacc = hjson.load(open(f"{__PATH}/timeacc.json", 'r'))
 # pipeline
 angacc = hjson.load(open(f"{__PATH}/angacc.json", 'r'))
 
+# User {{{
+
+#  path : Sidecar is the folder where all tuples will be placed, and 
+#         from rules are fed from.
+#  
+#  Sidecar path --------------------------------------------------------------
+#     Sidecar is the folder where all tuples will be placed, and from rules
+#     are fed from.
+#  
+#  EOS path ------------------------------------------------------------------
+#     This is the EOS folder where all tuples for the Run2 phis analysis are
+#     placed.
+#  
+#  Mail list -----------------------------------------------------------------
+#     If a rule runs succesfully, its log will be forwarded to all the emails
+#     in the following list (by default it is empty).
 
 user = hjson.load(open(f"{__PATH}/user.json", 'r'))
 
+# }}}
 
 base = hjson.load(open(f"{__PATH}/base.json", 'r'))
+blinding = hjson.load(open(f"{__PATH}/blinding.json", 'r'))
+general = hjson.load(open(f"{__PATH}/globals.json", 'r'))
 
+"""
 
+  // Gd value ------------------------------------------------------------------
+  //    This value is shared by all steps of this analysis, so a change here
+  //    would imply a change in the whole set of results. By default this value
+  //    is set to the World Average
+  "Gd_value": 0.65789, //1/ps
+  //
+  // Toys ----------------------------------------------------------------------
+  //    Number of toys to generate
+  "ntoys": 4000,
+  //
+  // Cuts in decay-time --------------------------------------------------------
+  //    When doing computations, some cuts for decay-time are applied, both to
+  //    low decay-times (removing prompt background) and to higher decay-times
+  //    (since there there is no statistics)
+  "tLL": 0.3, // ps
+  "tUL": 15.0, // ps
+"""
