c38167c28207dd2161eda9dfd8b75f0c088908ed
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2021-10-13 09:49:30 +0000

    add script to split time in 2 regions of the same eff stat

--- miscelaneous/compute_best_time_split_value.py
+++ miscelaneous/compute_best_time_split_value.py
@@ -26,11 +26,30 @@ def equibins1d(x, nbin):
 
 
 all_data = pd.concat([
-  uproot.open('/scratch46/marcos.romero/sidecar/2015/Bs2JpsiPhi/v0r5.root')['DecayTree'].pandas.df(branches='time'),
-  uproot.open('/scratch46/marcos.romero/sidecar/2016/Bs2JpsiPhi/v0r5.root')['DecayTree'].pandas.df(branches='time'),
-  uproot.open('/scratch46/marcos.romero/sidecar/2017/Bs2JpsiPhi/v0r5.root')['DecayTree'].pandas.df(branches='time'),
-  uproot.open('/scratch46/marcos.romero/sidecar/2018/Bs2JpsiPhi/v0r5.root')['DecayTree'].pandas.df(branches='time')
+  uproot.open('/scratch46/marcos.romero/sidecar/2015/Bs2JpsiPhi/v0r5_sWeight.root')['DecayTree'].pandas.df(),
+  uproot.open('/scratch46/marcos.romero/sidecar/2016/Bs2JpsiPhi/v0r5_sWeight.root')['DecayTree'].pandas.df(),
+  uproot.open('/scratch46/marcos.romero/sidecar/2017/Bs2JpsiPhi/v0r5_sWeight.root')['DecayTree'].pandas.df(),
+  uproot.open('/scratch46/marcos.romero/sidecar/2018/Bs2JpsiPhi/v0r5_sWeight.root')['DecayTree'].pandas.df()
 ])
 
 
 print(equibins1d(np.array(all_data['time']), 2))
+
+t = np.array(all_data['time'])
+sw = np.array(all_data['sw'])
+sw *= np.sum(sw)/np.sum(sw**2)
+
+all_ok = False
+meh, bins = np.histogram(t, weights=sw, bins=2)
+print(bins)
+
+while not all_ok :
+    if meh[0]-meh[1] > 2:
+        bins[1] -= 1e-2   
+    elif meh[1]-meh[0] > 2:
+        bins[1] += 1e-2   
+    else:
+        all_ok = True
+    meh, bins = np.histogram(t, weights=sw, bins=bins)
+    print(meh, bins)
+
