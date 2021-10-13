7dc6b8f6efa97d735ae1dd78683a51b23a17bed5
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2021-10-13 15:27:23 +0000

    rename file

--- analysis/lifetime/lifetime_fit_in_mass_bins.py
+++ analysis/lifetime/lifetime_fit_in_mass_bins.py
@@ -9,12 +9,13 @@ import matplotlib.pyplot as plt
 from ipanema import Sample, optimize, ristra, Parameters
 
 from selection.mass_fit.bd_mc import mass_fitter
-from utils.strings import printsubsec
+from utils.strings import printsec, printsubsec
 from trash_can.knot_generator import create_time_bins
 
 tau = unc.ufloat(1.520, 0.004)
 
 if __name__ == '__main__':
+  printsec("Lifetime crappy estimation")
   p = argparse.ArgumentParser(description="adfasdf")
   p.add_argument("--rd-sample")
   p.add_argument("--mc-sample")
@@ -32,13 +33,15 @@ if __name__ == '__main__':
   output_figures = args['output_figures']
   os.makedirs(output_figures, exist_ok=True)
 
+
   number_of_time_bins = 50
   mass_branch = 'B_ConstJpsi_M_1'
-  time_bins = np.linspace(0.3, 15, number_of_time_bins+1)[:-1]
+
+
+  time_bins = create_time_bins(number_of_time_bins, 0.4, 12)
   print(time_bins)
-  time_bins = create_time_bins(number_of_time_bins, 0.4, 10)
   time_bins[-2] = time_bins[-1]
-  time_bins = time_bins[:-1]
+  time_bins = np.array(time_bins[:-1])
 
   # Compute the efficiency
   printsubsec("Compute the efficiency")
@@ -47,10 +50,11 @@ if __name__ == '__main__':
   mc = Sample.from_root(args['mc_sample'])
   rd = Sample.from_root(args['rd_sample'])
   # rd_hist = np.histogram(rd.df['time'], bins=time_bins, density=True)[0]
-  mc_hist = np.histogram(mc.df['time'], bins=time_bins, density=True)[0]
-  # mc_hist = np.histogram(mc.df['time'], bins=time_bins, density=False)[0]
-  # mc_sum = np.sum(mc_hist)
-  print("MC:", mc_hist)
+  _mc_hist = np.histogram(mc.df['time'], bins=time_bins, density=True)[0]
+  mc_hist = np.histogram(mc.df['time'], bins=time_bins, density=False)[0]
+  mc_sum = mc_hist/_mc_hist
+  print(mc_hist/mc_sum)
+  print("MC:", mc_hist/mc_sum, _mc_hist)
 
   # compute the prediction for each bin
   pred = []
@@ -60,54 +64,66 @@ if __name__ == '__main__':
     pred.append(integral_s - integral_f) 
   print("TOY:", pred)
 
-  # eff = unp.uarray(mc_hist/pred, 0*np.sqrt(mc_hist)/pred)/mc_sum
-  eff = unp.uarray(mc_hist, 0*np.sqrt(mc_hist)/pred)
-  print("eff:", eff)
+  _eff = unp.uarray(mc_hist/pred, 0*np.sqrt(mc_hist)/pred)/mc_sum
+  # eff = unp.uarray(mc_hist, 0*np.sqrt(mc_hist)/pred)
+  print("eff:", _eff)
 
 
   printsubsec("Mass fit loop")
-  nevts = []
+  nevts = []; tbins = []; eff = []; i = 0
   for ll, ul in zip(time_bins[:-1], time_bins[1:]):
     print(f"Bd mass fit in {ll}-{ul} ps time window")
     cdf = rd.df.query(f"time>{ll} & time<{ul}")
     # do mass fit here
-    cpars = mass_fitter(cdf, figs=output_figures, trigger=trigger, verbose=False, label=f'{ll}-{ul}')
-    nevts.append(cpars['nsig'].uvalue) 
-    print(nevts)
-    print(cpars)
+    try:
+      cpars = mass_fitter(cdf, figs=output_figures, trigger=trigger, verbose=False, label=f'{ll}-{ul}')
+      nevts.append(cpars['nsig'].uvalue) 
+      tbins.append( np.median(rd.df.query(f'time>{ll} & time>{ul}')['time']) )
+      eff.append(_eff[i])
+      # print(nevts[-1])
+      print(cpars)
+    except:
+      print('  - fit failed')
+    i += 1
+  print(f"Succesfully fitted {len(nevts)} time bins")
+  nevts = nevts[2:]
+  tbins = tbins[2:]
+  eff = eff[2:]
 
+  nevts = nevts[:-2]
+  tbins = tbins[:-2]
+  eff = eff[:-2]
 
   if number_of_time_bins>5:
     printsubsec("Lifetime fit")
-    _x = 0.5*(time_bins[1:]+time_bins[:-1])
-    __y = unp.uarray([v.n for v in nevts], [v.s for v in nevts]) * eff
-    print(__y)
-    _y = unp.nominal_values(__y)
-    _uy = unp.std_devs(__y)
+    _x = np.array(tbins)
+    _y = unp.uarray([v.n for v in nevts], [v.s for v in nevts])
+    _y *= unp.uarray([v.n for v in eff], [v.s for v in eff])
+    _y, _uy = unp.nominal_values(_y), unp.std_devs(_y)
     _wy = 1/_uy**2
 
     print(_x, _y, _wy)
-    plt.plot(_x, _y, 'o')
+    # plt.plot(_xx, _y, 'o')
     plt.errorbar(_x, _y, yerr=_uy, fmt='.', color='k') 
-    plt.savefig("meh.pdf")
-    print("please cook fitter here")
 
     def fcn(pars, x, y=None, uy=False):
-        _pars = pars.valuesdict()
-        _model = _pars['N'] * np.exp( -x/_pars['tau'] )
-        if y is not None:
-            return ((y - _model) / uy)**2
-        return _model
+      _pars = pars.valuesdict()
+      _model = _pars['N'] * np.exp( -x/_pars['tau'] )
+      if y is not None:
+          return ((y - _model) / uy)**2
+      return _model
 
-    
+    # Create parameters and fit 
     pars = Parameters()
-    pars.add(dict(name="tau", value=1.0))
+    pars.add(dict(name="tau", value=1.0, min=1.35, max=1.85))
     pars.add(dict(name="N", value=150))
     result = optimize(fcn, params=pars, method='minuit', fcn_kwgs={"x":_x, "y":_y, "uy":_uy}, verbose=True)
-
+    print(result)
+    
+    # Dump Bx decay-width to json
     __gamma = 1/result.params['tau'].uvalue
     gamma = Parameters()
-    gamma.add(dict(name="gamma", value=__gamma.n, stdev=__gamma.s))
+    gamma.add(dict(name="gamma", value=__gamma.n, stdev=__gamma.s ))
     gamma.dump(args['output_params'])
 
     # plot
