9c784b942a4e70927b1e0f9471b335c1bef0f2fc
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2022-01-19 12:49:53 +0000

    style: clean up a bit the code

--- analysis/samples/tuple_stat.py
+++ analysis/samples/tuple_stat.py
@@ -11,10 +11,10 @@ __all__ = []
 
 # Modules {{{
 
-import argparse
-from ipanema import Sample
-from utils.strings import printsec
 import config
+from utils.strings import printsec
+from ipanema import Sample
+import argparse
 
 # }}}
 
@@ -22,44 +22,45 @@ import config
 # Command line runner {{{
 
 if __name__ == "__main__":
-  p = argparse.ArgumentParser(description=DESCRIPTION)
-  p.add_argument('--sample', help='Sample or samples to compute stats from')
-  p.add_argument('--year', help='Years')
-  p.add_argument('--output', help='Path to write the table')
-  args = vars(p.parse_args())
+    p = argparse.ArgumentParser(description=DESCRIPTION)
+    p.add_argument('--sample', help='Sample or samples to compute stats from')
+    p.add_argument('--year', help='Years')
+    p.add_argument('--output', help='Path to write the table')
+    args = vars(p.parse_args())
 
-  printsec("Sample statistics")
+    printsec("Sample statistics")
 
-  # Prepare data and totals
-  data = {}
-  totals = [0, 0, 0]
-  for i, y in enumerate(config.years[args['year']]):
-    this = Sample.from_root(args['sample'].split(',')[i])
-    data[y] = [
-      this.df.query(f'hlt1b==1').shape[0],
-      this.df.query(f'hlt1b==0').shape[0],
-      this.df.shape[0],
-    ]
-    totals[0] += data[y][0]
-    totals[1] += data[y][1]
-    totals[2] += data[y][2]
+    # Prepare data and totals
+    data = {}
+    totals = [0, 0, 0]
+    for i, y in enumerate(config.years[args['year']]):
+        this = Sample.from_root(args['sample'].split(',')[i])
+        data[y] = [
+            this.df.query('hlt1b==1').shape[0],
+            this.df.query('hlt1b==0').shape[0],
+            this.df.shape[0],
+        ]
+        totals[0] += data[y][0]
+        totals[1] += data[y][1]
+        totals[2] += data[y][2]
 
-  # Create table and write it
-  line = "{:>6} & {:>8} & {:>8} & {:>8} \\\\"
-  table = []
-  table.append(r'\begin{tabular}{lccc}')
-  table.append(r'\toprule')
-  table.append(rf"{'year':>6} & {'biased':>8} & {'unbiased':>8} & {'total':>8} \\")
-  table.append("\midrule")
-  for y, d in data.items():
-    table.append(line.format(*[y,*d]))
-  table.append("\midrule")
-  table.append(line.format(*['total',*totals]))
-  table.append(r"\bottomrule")
-  table.append(r"\end{tabular}")
-  with open(args['output'], 'w') as fp:
-    fp.write('\n'.join(table))
-  print('\n'.join(table))
+    # Create table and write it
+    line = "{:>6} & {:>8} & {:>8} & {:>8} \\\\"
+    table = []
+    table.append(r'\begin{tabular}{lccc}')
+    table.append(r'\toprule')
+    table.append(
+        rf"{'year':>6} & {'biased':>8} & {'unbiased':>8} & {'total':>8} \\")
+    table.append(r"\midrule")
+    for y, d in data.items():
+        table.append(line.format(*[y, *d]))
+    table.append(r"\midrule")
+    table.append(line.format(*['total', *totals]))
+    table.append(r"\bottomrule")
+    table.append(r"\end{tabular}")
+    with open(args['output'], 'w') as fp:
+        fp.write('\n'.join(table))
+    print('\n'.join(table))
 
 # }}}
 
