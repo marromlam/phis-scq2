f36a1dda8a82c8fa44a4b6e84dadd886b7b6be96
Marcos Romero Lamas <mromerol@cern.ch>
2022-02-23 13:03:02 +0100

    feat: clean time accepntace module

---
+++ analysis/time_acceptance/__init__.py
@@ -0,0 +1,17 @@
+"""
+Time acceptance 
+
+
+"""
+
+
+__all__ = ['simultaneous', 'single']
+__author__ = ["name"]
+__email__ = ["email"]
+
+
+from . import simultaneous
+from . import single
+
+
+# vim: fdm=marker ts=2 sw=2 sts=2 sr noet
