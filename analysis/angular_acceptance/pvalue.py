ea8e09cf6e8b93d921404cf109ca38339005948c
Ramon Angel Ruiz Fernandez <ramon.angel.ruiz.fernandez@cern.ch>
2022-05-10 11:54:50 +0200

    feat: make pvalues.py compatible w/ rules MC check

--- analysis/angular_acceptance/pvalue.py
+++ analysis/angular_acceptance/pvalue.py
@@ -25,8 +25,8 @@ def pvalue(pars, genpars, names=None):
 
 if __name__ == '__main__':
   p = argparse.ArgumentParser(description='overall p-value for Angular acceptance after convergence')
-  p.add_argument('--input', help='Params from angular acceptance')
-  p.add_argument('--params', help='Generated parameters')
+  p.add_argument('--target', help='Params from angular acceptance')
+  p.add_argument('--original', help='Generated parameters')
   p.add_argument('--year', help='Year of the sample')
   p.add_argument('--mode', help='Mode fitted')
   p.add_argument('--version', help='Version and cuts fitted')
@@ -37,8 +37,8 @@ if __name__ == '__main__':
   YEAR = args['year']
   MODE = args['mode']
   output = args['output']
-  pars = Parameters.load(args['input'])
-  gen_pars = Parameters.load(args['params'])
+  pars = Parameters.load(args['target'])
+  gen_pars = Parameters.load(args['original'])
 
   names = [key for key in pars.keys() if pars[key].free==True]
   if 'Bd2JpsiKstar' in MODE:
@@ -49,19 +49,32 @@ if __name__ == '__main__':
   value = np.array([pars[key].value for key in pars.keys()])
   stdev = np.array([pars[key].stdev for key in pars.keys()])
   genvalue = np.array([gen_pars[key].value for key in pars.keys()])
-  PULL = (value-genvalue)/stdev
+  genstdev = np.array([gen_pars[key].stdev for key in pars.keys()])
+  Comb = False
+  if None in genstdev:
+    comb = stdev
+  else:
+    Comb = True
+    comb = np.sqrt(genstdev**2+stdev**2)
+  PULL = (value-genvalue)/comb
   if 'Bd2JpsiKstar' in MODE:
     pvalue = pvalue(pars, gen_pars, names=names)
   else:
     pvalue = pvalue(pars, gen_pars)
+  print(pvalue)
 
   #Latex table
   table = []
   table.append(r"\begin{tabular}{c|cc}")
   table.append(r"\toprule")
-  col1 = 'Values Obtained (stats only)'
+  col1 = 'Values (stats only)'
   col2 = 'Pulls (stats only)'
-  table.append(f"{'Parameters':<40} & {col1:>25} &  {col2:>17} \\\\")
+  if Comb:
+    col3 = col2
+    col2 = 'GenValues'
+    table.append(f"{'Parameters':<40} & {col1:>25} & {col2:>25} & {col3:>17} \\\\")
+  else:
+    table.append(f"{'Parameters':<40} & {col1:>25} &  {col2:>17} \\\\")
   table.append(r"\midrule")
   for i in range(len(names)):
     line = []
@@ -69,6 +82,9 @@ if __name__ == '__main__':
     param = f"{unc.ufloat(value[i], stdev[i]):.2uL}"
     nsigma = PULL[i]
     line.append(f" ${param:>26}$ ")
+    if Comb:
+      genparam = f"{unc.ufloat(genvalue[i], genstdev[i]):.2uL}"
+      line.append(f" ${genparam:>26}$ ")
     line.append(f"    $ {nsigma:>+.2f}\sigma $  ")
     table.append("&".join(line)+r"\\")
   table.append(r"\midrule")
