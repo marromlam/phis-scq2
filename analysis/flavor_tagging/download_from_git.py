870357257f398ddb3d87730ff19a760887f76646
Ramon Angel Ruiz Fernandez <ramon.angel.ruiz.fernandez@cern.ch>
2023-02-28 11:14:34 +0100

    Updates tagging calibration parameters

--- analysis/flavor_tagging/download_from_git.py
+++ analysis/flavor_tagging/download_from_git.py
@@ -40,6 +40,9 @@ if __name__ == "__main__":
   out_path = args['output']
   linker = args['linker']
   repo = args['repo']
+  
+  if "v5r" not in version:
+    version = "v4r0"
 
   if version in ('v0r0', 'v0r1', 'v0r2', 'v0r3', 'v0r4'):
     printsec("Plugging old flavor factors")
@@ -53,7 +56,6 @@ if __name__ == "__main__":
 
     # get name for time resolution
     _flavor = hjson.load(open(linker, "r"))[flavor].format(year=year)
-    print(_flavor)
 
     # cook local and remote paths
     tmp_path = out_path.replace('output', 'tmp')
@@ -62,17 +64,54 @@ if __name__ == "__main__":
     if 'peilian' in flavor:
         git_path = f"fitinputs/{version}/tagging_calibration/"
     os.makedirs(tmp_path, exist_ok=True)
+    
+    print(version)
+    if ("peilian" in flavor) and ('v5r' in version):
+      if "2015" in year:
+        year = "2016"
+      git_path = f"fitinputs/v4r0/tagging_calibration/"
+      outd = Parameters.load("analysis/params/flavor_tagging/Bs2JpsiPhi/none.json")
+      for tag in ["OS", "SSK"] :
+        _flavor = f"ExampleCombinationOutputResultSet{tag}-v4r0.json"
 
-    print(f'Downloading Bs2JpsiPhi-FullRun2: {git_path}/{_flavor}')
-    os.system(f"git archive --remote={repo} --prefix=./{tmp_path}/ HEAD:{git_path} {_flavor} | tar -x")
+        print(f'Downloading Bs2JpsiPhi-FullRun2: {git_path}/{_flavor}')
+        os.system(f"git archive --remote={repo} --prefix=./{tmp_path}/ HEAD:{git_path} {_flavor} | tar -x")
 
-    print(f"Loading flavor factors {year}")
-    raw_json = hjson.load(open(f"{tmp_path}/{_flavor}",'r'))
-    rawd = raw_json['TaggingParameter']
-    outd = Parameters.load("analysis/params/flavor_tagging/Bs2JpsiPhi/none.json")
+        print(f"Loading flavor factors {year}")
+        raw_json = hjson.load(open(f"{tmp_path}/{_flavor}",'r'))
+        rawd = raw_json['ResultSet'][0]["Parameter"]
   
-    print(f'Parsing parameters to match phis-scq sctructure')
-    for i, par in enumerate(list(outd.keys())):
+        print(f'Parsing parameters to match phis-scq sctructure')
+        for i, par in enumerate(list(outd.keys())):
+          for _i in range(len(rawd)):
+            if (rawd[_i]['Name'][:5] == f'{par[:2]}_'+f'{par[3:]}'.upper()) and (year in rawd[_i]["Name"]):
+              outd[par].set(value=rawd[_i]['Value'], stdev=rawd[_i]['Error'])
+              outd[par].correl = {}
+              for j in range(0,3):
+                pi = par[:2]; pj = f"p{j}"; tag = par[3:]
+                for k in range(len(rawd)):
+                  if rawd[k]['Name'][:12].lower() == f'rho_{pi}_{pj}_{tag}':
+                    outd[par].correl[f"{pj}_{tag}"] = rawd[k]['Value']
+                  elif rawd[k]['Name'][:12].lower() == f'rho_{pj}_{pi}_{tag}':
+                    outd[par].correl[f"{pj}_{tag}"] = rawd[k]['Value']
+
+            elif (rawd[_i]['Name'][:6] == f'{par[:3]}_'+f'{par[4:]}'.upper()) and (year in rawd[_i]["Name"]):
+              outd[par].set(value=rawd[_i]['Value'], stdev=rawd[_i]['Error'])
+
+        outd["eta_os"].set(value=0.3546, stdev=0.)
+        outd["eta_ss"].set(value=0.42388, stdev=0.)
+
+    else:
+      print(f'Downloading Bs2JpsiPhi-FullRun2: {git_path}/{_flavor}')
+      os.system(f"git archive --remote={repo} --prefix=./{tmp_path}/ HEAD:{git_path} {_flavor} | tar -x")
+
+      print(f"Loading flavor factors {year}")
+      raw_json = hjson.load(open(f"{tmp_path}/{_flavor}",'r'))
+      rawd = raw_json['TaggingParameter']
+      outd = Parameters.load("analysis/params/flavor_tagging/Bs2JpsiPhi/none.json")
+  
+      print(f'Parsing parameters to match phis-scq sctructure')
+      for i, par in enumerate(list(outd.keys())):
         for _i in range(len(rawd)):
           if rawd[_i]['Name'][:5] == f'{par[:2]}_'+f'{par[3:]}'.upper():
             outd[par].set(value=rawd[_i]['Value'], stdev=rawd[_i]['Error'])
